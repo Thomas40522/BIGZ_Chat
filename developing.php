592317054e2076ebe1d1b9b82526a1c8ca1baa61
Thomas40522 <73463155+Thomas40522@users.noreply.github.com>
2022-01-21 20:25:21 +0800

    f

--- developing.php
+++ developing.php
@@ -3,6 +3,7 @@
     <head>
         <meta charset="utf-8"/>
         <link rel="stylesheet" href="css/navbar.css">
+        <link rel="stylesheet" href="css/developing.css">
         <title>BIGZ chat</title>
     </head>
     <body>
@@ -10,17 +11,17 @@
         <nav class="nav_bar">
             <a href="index.php"><span id = "title">BIGZ Chat</span></a>
             <a href="titlepage.php"><span id = "front">首页</span></a>
-            <a href="developing.php"><span id = "hot">热点</span></a>
-            <a href="developing.php"><span id = "public"> 公告栏</span></a>
-            <a href="developing.php"><span id = "box"> 意见箱</span></a>
-            <a href="developing.php"><span id = "aboutus">About Us</span></a>
+            <a href=""><span id = "hot">热点</span></a>
+            <a href=""><span id = "public"> 公告栏</span></a>
+            <a href=""><span id = "box"> 意见箱</span></a>
+            <a href=""><span id = "aboutus">About Us</span></a>
             <user>
                 <img id = "profilePic" src = "assests/ProfilePic.png", width = "40px", height = "40px", alt = "profile picture">
                 <span id = "username"></span>
             </user>
         </nav>
         </header>
-
+        <div id="red">I am sorry to inform you that this function is currently developing...</div>
         <script src="js/titlepage.js"></script>
     </body>
 </html>
--- reply.php
+++ reply.php
@@ -25,10 +25,10 @@
         <nav class="nav_bar">
             <a href="index.php"><span id = "title">BIGZ Chat</span></a>
             <a href="titlepage.php"><span id = "front">首页</span></a>
-            <a href=""><span id = "hot">热点</span></a>
-            <a href=""><span id = "public"> 公告栏</span></a>
-            <a href=""><span id = "box"> 意见箱</span></a>
-            <a href=""><span id = "aboutus">About Us</span></a>
+            <a href="developing.php"><span id = "hot">热点</span></a>
+            <a href="developing.php"><span id = "public"> 公告栏</span></a>
+            <a href="developing.php"><span id = "box"> 意见箱</span></a>
+            <a href="developing.php"><span id = "aboutus">About Us</span></a>
             <user>
                 <img id = "profilePic" src = "assests/ProfilePic.png", width = "40px", height = "40px", alt = "profile picture">
                 <span id = "username"></span>
